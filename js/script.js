05f90a96a185f227bfc22b6213f6b81cd716dae5
ChloeLiang <nliangxin@gmail.com>
2021-02-19 19:08:56 +0800

    migrate to hugo

--- js/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-  /* Scroll on button */
-  $('.js--scroll-to-portfolio').click(function() {
-    $('html, body').animate(
-      { scrollTop: $('.section-portfolio').offset().top },
-      800
-    );
-  });
-});
